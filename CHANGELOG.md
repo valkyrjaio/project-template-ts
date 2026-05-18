062d17023ffdf0e0121673bd6c628eeb58f3c6b8
Melech Mizrachi <melechmizrachi@gmail.com>
2026-05-17 22:46:40 -0700

    [Initial] Initial commit.

---
+++ CHANGELOG.md
@@ -0,0 +1,3 @@
+# Release Notes for 1.x
+
+## [Unreleased](https://github.com/valkyrjaio/ts-template/compare/v1.0.0...master)
---
+++ src/Template/Constant/TemplateInfo.ts
@@ -0,0 +1,4 @@
+export class TemplateInfo {
+    static readonly VERSION = '1.0.0' as const;
+    static readonly VERSION_BUILD_DATE_TIME = 'January 1 2025 00:00:00 MST' as const;
+}
